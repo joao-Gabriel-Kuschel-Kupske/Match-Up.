8a4eff6f901d66ae446d5e6db86faf81a6897a2a
João Gabriel Kuschel Kupske <joaogabrielkk2@gmail.com>
2025-10-29 23:17:34 +0000

    iniciação

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, redirect, url_for
+import csv
+from datetime import datetime
+app = Flask(__name__)
+@app.get('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
