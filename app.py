08e0f9727418f6c11a63ebe5b6e20ac4cf0f3fe1
João Gabriel Kuschel Kupske <joaogabrielkk2@gmail.com>
2025-10-30 00:54:19 +0000

    começando

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 import csv
 from datetime import datetime
 app = Flask(__name__)
-@app.get('/')
+@app.get("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
