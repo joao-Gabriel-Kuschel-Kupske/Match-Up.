d2196d4c13731d6fa525ca42384b4b8ab0baa7bb
João Gabriel Kuschel Kupske <joaogabrielkk2@gmail.com>
2025-11-04 00:01:34 +0000

    back_e_paginas_de_aulas

--- match.up/app.py
+++ match.up/app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
-import csv
-from datetime import datetime
+
 app = Flask(__name__)
-@app.get("/")
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-    
+@app.route('/divisao_euclidiana')
+def divisao_euclidiana():
+    return render_template("divisao.html")
+
+@app.route('/multiplos_e_divisores')
+def multiplos_e_divisores():
+    return render_template("multiplos_e_div.html")
+
+@app.route('/fracao')
+def fracao():
+    return render_template("fração.html")
+
+@app.route('/introducao_a_geometria')
+def introducao_a_geometria():
+    return render_template("geometria.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
